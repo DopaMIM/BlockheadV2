c4ad420848450fd08ba73f6026c4cb197a5773e4
Levi Webb <levi@MacBook-Air.local>
2022-06-20 15:15:59 -0700

    config update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,55 +2,55 @@ module.exports = {
   apps : [
     {
     name   : "LINK",
-    script : "./LINK.js"},
+    script : "LINK.js"},
 
     { 
     name   : "AAVE",
-    script : "./AAVE.js"
+    script : "AAVE.js"
     },
     { 
       name   : "BAL",
-      script : "./BAL.js"
+      script : "BAL.js"
     },
     { 
       name   : "camAAVE",
-      script : "./camAAVE.js"
+      script : "camAAVE.js"
     },
     { 
       name   : "camWBTC",
-      script : "./camWBTC.js"
+      script : "camWBTC.js"
     },
     { 
       name   : "camWETH",
-      script : "./camWETH.js"
+      script : "camWETH.js"
     },
     { 
       name   : "CRV",
-      script : "./CRV.js"
+      script : "CRV.js"
     },
     { 
       name   : "dQUICK",
-      script : "./dQUICK.js"
+      script : "dQUICK.js"
     },
     { 
       name   : "GHST",
-      script : "./GHST.js"
+      script : "GHST.js"
     },
     { 
       name   : "MATIC",
-      script : "./MATIC.js"
+      script : "MATIC.js"
     },
     { 
       name   : "WBTC",
-      script : "./WBTC.js"
+      script : "WBTC.js"
     },
     { 
       name   : "WETH",
-      script : "./WETH.js"
+      script : "WETH.js"
     },
     { 
       name   : "FTM",
-      script : "./FTM.js"
+      script : "FTM.js"
     }
   ]
 }

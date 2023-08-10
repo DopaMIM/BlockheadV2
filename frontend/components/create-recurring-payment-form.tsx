d2f54f033f3293b4993f28aa397448e28ff74f91
Jason Dubaniewicz <jason@Jasons-MacBook-Pro-2.local>
2023-08-10 02:10:49 -0700

    Frontend - initial commit

---
+++ frontend/components/create-recurring-payment-form.tsx
@@ -0,0 +1,157 @@
+"use client";
+import {useRecurringPaymentContract} from "@/lib/use-recurring-payment-contract";
+import {utils, Contract, BigNumber} from "ethers";
+import {Icons} from "@/components/icons";
+import {MetaMaskConnectButton} from "@/components/metamask-connect-button";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {Badge} from "@/components/ui/badge";
+import {Button, buttonVariants} from "@/components/ui/button";
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {Separator} from "@/components/ui/separator";
+import {
+  LEVI_TEST_WALLET_ADDRESS,
+  RECURRING_PAYMENT_CONTRACT,
+  SepoliaTestNetwork,
+  UI_PARTNER_ADDRESS
+} from "@/constants";
+import {cn, formatAmount, frequencyToSeconds, getTokenAddress, parseAmount} from "@/lib/utils";
+import {recurringPaymentsABI} from "@/lib/recurring-payments-abi";
+import type {Subscription} from "@/lib/validations/subscription";
+import {Polygon, useEthers, useCall, useToken, useTokenAllowance, ERC20Interface, useSigner} from "@usedapp/core";
+import Link from "next/link";
+import {useState} from "react";
+
+interface ICreateRecurringPaymentForm {
+  subscription: Subscription
+}
+export default function CreateRecurringPaymentForm({subscription}: ICreateRecurringPaymentForm) {
+  const {account, error, switchNetwork} = useEthers()
+  const signer = useSigner()
+  const [loading, setLoading] = useState(false)
+  
+  if (error?.name === 'ChainIdError') {
+    switchNetwork(SepoliaTestNetwork.chainId)
+  }
+  
+  const subscriptionTokenAddress = getTokenAddress(subscription.meta.token)
+  const amountInWei = parseAmount(subscription.meta.amount, subscriptionTokenAddress)
+  console.log('amountInWei', amountInWei.toString())
+  
+  const recurringPaymentContract = useRecurringPaymentContract()
+  
+  console.log('subscription', subscription, frequencyToSeconds(subscription.meta.frequency))
+  const subscriptionToken = new Contract(subscriptionTokenAddress, ERC20Interface, signer)
+  
+  // check allowance for this subscription's token
+  const allowance = useTokenAllowance(subscriptionTokenAddress, account, RECURRING_PAYMENT_CONTRACT)
+  console.log('allowance', allowance)
+  
+  // if less than the subscription amount, show a message to approve the token
+  async function approveToken() {
+    setLoading(true)
+    if (!allowance && subscriptionToken) {
+      await subscriptionToken.approve(RECURRING_PAYMENT_CONTRACT, parseAmount('10000000', subscriptionTokenAddress))
+    }
+    setLoading(false)
+  }
+  
+  async function approveSubscription() {
+    setLoading(true)
+    if (!subscriptionToken || !recurringPaymentContract) {
+      return
+      setLoading(false)
+    }
+    
+    const functionArgs = [
+      subscription.meta.receiverAddress,
+      amountInWei,
+      subscriptionTokenAddress,
+      frequencyToSeconds(subscription.meta.frequency),
+      UI_PARTNER_ADDRESS,
+      [`subscriptionId:${subscription.id}`],
+      frequencyToSeconds(subscription.meta.trial),
+    ]
+    
+    const gasLimit = await recurringPaymentContract.estimateGas.createRecurringPayment(...functionArgs)
+    //@ts-ignore
+    const {value, error} = await recurringPaymentContract.createRecurringPayment(
+      ...functionArgs,
+      // overrides
+      { gasLimit }
+    )
+    
+    if (error) {
+      console.error(error.message)
+      return
+    }
+    console.log('value', value.toString())
+    
+    setLoading(false)
+  }
+  
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 max-w-2xl">
+        <div className="flex flex-col space-y-8 text-center">
+          <div>
+          <div className="flex space-x-2 justify-center">
+            <h1 className="mt-16 text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+              {subscription.meta.recipientName}
+            </h1>
+          </div>
+          <p className="text-lg text-gray-500 sm:text-xl">
+            {subscription.meta.productName}
+          </p>
+          </div>
+          <div className="grid w-full items-start gap-6 rounded-lg border p-6">
+            <div className="grid gap-6">
+              <h3 className="text-xl font-bold sm:text-2xl">
+                Setup recurring payment?
+              </h3>
+              <ul className="grid gap-3 text-sm text-muted-foreground grid-cols-1">
+                <li className="flex flex-col sm:flex-row space-x-2">
+                  <span className="flex items-center shrink-0">
+                    <Icons.logo className="mr-2 h-4 w-4" /> Verify the receiver address
+                  </span>
+                  <span className="">
+                    <Badge variant="secondary">{subscription.meta.receiverAddress}</Badge>
+                  </span>
+                </li>
+                <li className="flex items-center">
+                  <Icons.logo className="mr-2 h-4 w-4" /> You can cancel at any time from your dashboard.
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col gap-4 text-center">
+              <div>
+                <h4 className="text-7xl font-bold">${subscription.meta.amount}</h4>
+                <p className="text-sm font-medium text-muted-foreground">
+                  {subscription.meta.token.toLocaleUpperCase()} billed {subscription.meta.frequency}
+                </p>
+                {subscription.meta?.trial !== 'none' && (
+                  <p className="text-sm font-medium text-muted-foreground">
+                    *first {subscription.meta?.trial} is free!
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
+          {!account ? (
+            <MetaMaskConnectButton />
+          ) : !BigNumber.isBigNumber(allowance) ? (
+            <Button variant="secondary" onClick={() => approveToken()}>Approve {subscription.meta.token.toLocaleUpperCase()}</Button>
+          ) : (
+            <Button type="submit" onClick={() => approveSubscription()}>Subscribe</Button>
+          )}
+          <p className="pb-16 px-8 text-center text-sm text-muted-foreground">
+            <Link href="/" className="hover:text-brand underline underline-offset-4">
+              Back to blockhead
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ frontend/components/outgoing-subscriptions.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import {Button, buttonVariants} from "@/components/ui/button";
+import {RECURRING_PAYMENT_CONTRACT} from "@/constants";
+import {recurringPaymentsABI} from "@/lib/recurring-payments-abi";
+import {useRecurringPaymentContract} from "@/lib/use-recurring-payment-contract";
+import {cn, formatAmount, getTokenAddress, getTokenName, paymentDueSecondsToDays} from "@/lib/utils";
+import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import {useEthers, useSigner} from "@usedapp/core";
+import {Contract, utils, BigNumber} from "ethers";
+import Link from "next/link";
+import React, {useEffect, useState} from "react";
+import _ from "lodash"
+
+const supabase = createClientComponentClient()
+
+function useRecurringPayments() {
+  const [loaded, setLoaded] = useState<boolean>(false)
+  const [recurringPayments, setRecurringPayments] = useState<Record<string, any>>({})
+  
+  const {account} = useEthers()
+  const signer = useSigner()
+  
+  let recurringPaymentContractInterface
+  let recurringPaymentContract
+  if (account && signer) {
+    recurringPaymentContractInterface = new utils.Interface(recurringPaymentsABI)
+    recurringPaymentContract = new Contract(RECURRING_PAYMENT_CONTRACT, recurringPaymentContractInterface, signer)
+  }
+  
+  useEffect(() => {
+    async function inner() {
+      try {
+        if (loaded) {
+          return
+        }
+        setLoaded(true)
+        setRecurringPayments({})
+        const currentBlockTimestamp = await recurringPaymentContract.getCurrentBlockTimestamp()
+        // find all payments for each subscription - get all payments for this user's address
+        const accountNumbers = await recurringPaymentContract.getAccountNumbersByAddress(account)
+        for (const accountNumber of accountNumbers) {
+          const [recurringPayment, additionalInformation] = await Promise.all([
+            recurringPaymentContract.recurringPayments(accountNumber),
+            recurringPaymentContract.getAdditionalInformation(accountNumber)
+          ])
+          if (recurringPayment[1] !== account) {
+            continue
+          }
+          const paymentDue = recurringPayment[7].sub(currentBlockTimestamp)
+          const subscriptionId = additionalInformation[0].split('subscriptionId:')[1]
+          const {data: subscription, error} = await supabase
+            .from('subscription')
+            .select('*')
+            .eq('id', subscriptionId)
+            .single()
+          if (error) {
+            throw error
+          }
+          const recurringPaymentArr = [...recurringPayment, subscription]
+          recurringPaymentArr[7] = paymentDue
+          recurringPayments[accountNumber.toString()] = recurringPaymentArr
+        }
+        setRecurringPayments(recurringPayments)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    
+    if (recurringPaymentContract) {
+      inner()
+    }
+  }, [recurringPaymentContract])
+  
+  // for (const subscription of data) {
+  //   subscription.payments = payments
+  // }
+  
+  return [recurringPayments]
+}
+
+export const OutgoingSubscriptions = () => {
+  const [recurringPayments] = useRecurringPayments()
+  const recurringPaymentContract = useRecurringPaymentContract()
+  
+  async function cancelRecurringPayment(accountNumber) {
+    if (!recurringPaymentContract) {
+      return
+    }
+    if (window.confirm('Are you sure you want to cancel this subscription?')) {
+      const gasLimit = await recurringPaymentContract.estimateGas.cancelRecurringPayment(accountNumber)
+      const ret = await recurringPaymentContract.cancelRecurringPayment(accountNumber, {gasLimit})
+      console.log(ret)
+    }
+  }
+  
+  return (
+    <div className="m-8 ">
+      <div className="flex items-center justify-between px-2">
+        <div className="flex items-center justify-between grow">
+          <h1 className="font-heading text-3xl md:text-4xl">Outgoing Subscriptions</h1>
+        </div>
+        {/*<p className="text-lg text-muted-foreground">Manage account and website settings.</p>*/}
+      </div>
+      <div className="mt-4 p-4 border border-gray-200 rounded-lg">
+        <div>
+          <div className="grid grid-cols-8 gap-2">
+            <div>
+              <span className="font-medium">recipient</span>
+            </div>
+            <div>
+              <span className="font-medium">product name</span>
+            </div>
+            <div>
+              <span className="font-medium">network</span>
+            </div>
+            <div>
+              <span className="font-medium">token</span>
+            </div>
+            <div>
+              <span className="font-medium">amount</span>
+            </div>
+            <div>
+              <span className="font-medium">next payment</span>
+            </div>
+            <div>
+              <span className="font-medium">Active ?</span>
+            </div>
+            <div></div>
+          </div>
+            {_.values(recurringPayments).map(([accountNumber, sender, recipientAddress, amount, token, timeIntervalSeconds, paymentInterface, paymentDue, cancelled, subscription]) => {
+              //const amountFormatted = BigNumber.from(amount)
+              //const amountToken = formatAmount(amount, token)
+              
+              return (
+                <div key={accountNumber.toString()} className="grid grid-cols-8 gap-2">
+                  <div className="">
+                    {/*<div className="">{id}</div>*/}
+                    <div className="">{subscription?.meta?.recipientName}</div>
+                    <div className="truncate">{recipientAddress}</div>
+                  </div>
+                  <div>{subscription?.meta?.productName}</div>
+                  <div>{subscription?.meta?.network}</div>
+                  <div className="truncate">{token.toUpperCase()}</div>
+                  <div className="truncate">{formatAmount(amount, token)} {getTokenName(token).toUpperCase()}</div>
+                  <div>{paymentDueSecondsToDays(paymentDue)} days</div>
+                  <div>{!cancelled ? 'Yes' : 'No'}</div>
+                  <div>
+                    {!cancelled && (<Button size="sm" onClick={() => cancelRecurringPayment(accountNumber)}>Cancel</Button>)}
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+      </div>
+    </div>
+  )
+}
